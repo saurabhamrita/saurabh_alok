522b265fb2c6194eb10ef019304e4951c699f53b
saurabhamrita <sk74564@gmail.com>
2025-11-21 15:38:33 +0530

    code push

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "naya_rg" {
+  name     = "naya-rg-pipeline11"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "naya_stg" {
+  name                     = "nayastgpipeline21"
+  resource_group_name      = azurerm_resource_group.naya_rg.name
+  location                 = azurerm_resource_group.naya_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
